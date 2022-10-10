ec4ecea4d5dfb0fd4a46b4a167d3fe29b4789e2a
Shreyask122 <shreyaskhamkar96@gmail.co>
2022-10-10 23:02:16 +0530

    Day 02

---
+++ Day 02/prob4.js
@@ -0,0 +1,12 @@
+//Problem 4: Given 3 numbers (all different values), print which is greatest
+
+let a=12;
+let b= 14;
+let c=16;
+if (a>b && a>c){
+  console.log("a is greatest");
+} else if(b>c && b>a){
+  console.log("b is greatest");
+}else {
+  console.log("c is greatest");
+}
---
+++ Day 02/prob5.js
@@ -0,0 +1,25 @@
+//Problem 5: Given the days of the week in short format "Sun", "Mon" ... print in long format "Sunday", "Monday", ...
+
+let day= "Mon";
+switch (day) {
+  case "Sun":
+    day = console.log("Sunday");
+    break;
+  case "Mon":
+    day = console.log("Monday");
+    break;
+  case "Tue":
+    day = console.lg("Tuesday");
+    break;
+  case "Wen":
+    day = console.log("Wednesday");
+    break;
+  case "Thu":
+    day = console.log("Thursday");
+    break;
+  case "Fri":
+    day = console.log("Friday");
+    break;
+  case  "Sat":
+    day = console.log("Saturday");
+}
